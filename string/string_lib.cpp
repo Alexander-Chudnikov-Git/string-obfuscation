aa4dfb741525285389f5eef9eb21e19a837d7195
RLS-Alexander-Chudnikov <admin@redline-software.xyz>
2022-10-04 22:29:59 +0300

    Thread unsafe implementation (dynamic arrays are not free)

---
+++ string/string_lib.cpp
@@ -0,0 +1,178 @@
+#include "string_lib.hpp"
+
+
+ObfuscatedString::ObfuscatedString()
+{
+
+}
+
+ObfuscatedString::~ObfuscatedString()
+{
+
+}
+
+/**
+ * @brief       Constexpr implemenatation of ctoi
+ * 
+ *              All credit goes to Mikhail Klimenko and his article "Constexpr pseudorandom numbers"
+ *              https://mklimenko.github.io/english/2018/06/04/constexpr-random/
+ *
+ * @param[in]   str     String, that shoild be converted to ineger
+ * @param[in]   offset  String offset
+ *
+ * @return      Converted integer
+ */
+constexpr unsigned int ObfuscatedString::constexpr_ctoi(const char* str, int offset)
+{
+    return static_cast<std::uint32_t>(str[offset] - '0') * 10 + static_cast<std::uint32_t>(str[offset + 1] - '0');
+}
+
+/**
+ * @brief       Constexpr implemenatation of creating seed from __TIME__ and __DATE__
+ * 
+ *              All credit goes to Mikhail Klimenko and his article "Constexpr pseudorandom numbers"
+ *              https://mklimenko.github.io/english/2018/06/04/constexpr-random/
+ *
+ * @return      Seed for pseudorandom generator
+ */
+constexpr unsigned int ObfuscatedString::constexpr_seed()
+{
+    auto compile_time = __TIME__;
+    auto compile_date = __DATE__;
+    return constexpr_ctoi(compile_time, 0) * 60 * 60 + constexpr_ctoi(compile_time, 3) * 60 + constexpr_ctoi(compile_time, 6) + constexpr_ctoi(compile_date, 5) * 24 * 60 * 60;
+}
+
+/**
+ * @brief       Constexpr uniform distribuion
+ *              
+ *              Previous value is passed by refeerence due to the lack of static values
+ *              
+ *              All credit goes to Mikhail Klimenko and his article "Constexpr pseudorandom numbers"
+ *              https://mklimenko.github.io/english/2018/06/04/constexpr-random/
+ *
+ * @param[in]   previous    Previous value
+ *
+ * @return      Uniformely distributed value in the range of [0, 714025]
+ */
+constexpr std::uint32_t ObfuscatedString::uniform_distribution(std::uint32_t &previous)
+{
+    previous = ((lce_a * previous + lce_c) % lce_m);
+    return previous;
+}
+
+/**
+ * @brief       Constexpr normalzed uniform distribuion
+ * 
+ *              Previous value is passed by refeerence due to the lack of static values
+ *              
+ *              All credit goes to Mikhail Klimenko and his article "Constexpr pseudorandom numbers"
+ *              https://mklimenko.github.io/english/2018/06/04/constexpr-random/
+ *
+ * @param       previous    Previous value
+ *
+ * @return      Uniformely distributed value in the range of [0.0, 1.0]
+ */
+constexpr double ObfuscatedString::uniform_distribution_n(std::uint32_t &previous)
+{
+    auto distribution = uniform_distribution(previous);
+    return static_cast<double>(distribution) / lce_m;
+}
+
+/**
+ * @brief       Constexpr uniform distribuion as an array
+ *              
+ *              All credit goes to Mikhail Klimenko and his article "Constexpr pseudorandom numbers"
+ *              https://mklimenko.github.io/english/2018/06/04/constexpr-random/
+ *
+ * @param[in]   min   Minimum distribution value
+ * @param[in]   max   Maximum distribution value
+ *
+ * @tparam      T     Typename of min/max value
+ * @tparam      SIZE  Size of normalized uniform distribution
+ *
+ * @return      std::array of uniformely distributed values in the range of [min, max] 
+ */
+template <typename T, std::size_t SIZE>
+    constexpr std::array<T, SIZE> ObfuscatedString::uniform_distribution(T min, T max)
+    {
+        std::array<T, SIZE> distribution{};
+        auto previous = constexpr_seed();
+        for (auto &element : distribution) 
+        {
+            element = static_cast<T>(uniform_distribution_n(previous) * (max - min) + min);
+        }
+        
+        return distribution;
+    }
+
+/**
+ * @brief       Constexpr normal distribuion as an array
+ *              
+ *              Normal distribution is being approximated by the Irwin-Hall distribution
+ *
+ *              All credit goes to Mikhail Klimenko and his article "Constexpr pseudorandom numbers"
+ *              https://mklimenko.github.io/english/2018/06/04/constexpr-random/
+ *
+ * @param[in]  mean         Irwin-Hall mean
+ * @param[in]  sigma        Irwin-Hall sigma
+ *
+ * @tparam     T            Typename of min/max value
+ * @tparam     SIZE         Size of normalized uniform distribution
+ * @tparam     IRWIN_NUM    Irwin distribution numbers
+ *
+ * @return     { description_of_the_return_value }
+ */
+template <typename T, std::size_t SIZE, std::size_t IRWIN_NUM>
+    constexpr std::array<T, SIZE> ObfuscatedString::normal_distribution()
+    {
+        std::array<T, SIZE> distribution{};
+        auto previous = constexpr_seed();
+        for (auto &element : distribution) 
+        {
+            double value = 0;
+            for (std::size_t i = 0; i < IRWIN_NUM; ++i)
+            {
+                value += uniform_distribution_n(previous);
+            }
+
+            element = value / std::sqrt(IRWIN_NUM / 12.0f) - IRWIN_NUM / 2.0f;
+        }
+
+        return distribution;
+    }
+
+
+/**
+ * @brief      Generation of random line with length of n by using uniform distribution
+ *
+ * @tparam     N     Length of generated line
+ *
+ * @return     Pseuorandom line with the lenght of N
+ */
+template<typename T, std::size_t N>
+    constexpr const T* ObfuscatedString::cgi_r()
+    {
+        T* key = new T[N];
+
+        constexpr auto distribution = uniform_distribution<int, N>(0, 255);
+
+        for (std::size_t i = 0; i < N; ++i) 
+        {
+            key[i] = distribution.at(i);
+        }
+
+        return key;
+    }
+
+template const char* ObfuscatedString::cgi_r<char, 64>();
+template std::array<int, 64> ObfuscatedString::uniform_distribution<int, 64>(int, int);
+template std::array<int, 64> ObfuscatedString::normal_distribution<int, 64, 12>();
+
+#undef __CGUI_OBF_KEY__
+#undef __CGUI_OBF__
+#undef __CGUI_OBF_S__
+
+#define __CGUI_OBF_KEY__ ObfuscatedString::cgi_r<char, 64>()
+#define __CGUI_OBF__(data) __CGUI_OBF_GENERIC__(data, __CGUI_OBF_KEY__)
+#define __CGUI_OBF_S__(data) __CGUI_OBF_GENERIC__(data, ObfuscatedString::cgi_r<char, 64>())
+
---
+++ string/string_lib.hpp
@@ -0,0 +1,156 @@
+#ifndef OBFUSCATED_STRING_HPP
+#define OBFUSCATED_STRING_HPP
+
+#include <cstring>
+#include <string>
+#include <memory>
+#include <random>
+#include <array>
+
+#include <iostream>
+
+class ObfuscatedString
+{
+public:
+	ObfuscatedString();
+	~ObfuscatedString();
+
+	template<typename T, std::size_t N>
+    	static constexpr const T* cgi_r();
+	template <typename T, std::size_t SIZE>
+		static constexpr std::array<T, SIZE> uniform_distribution(T min, T max);
+	template <typename T, std::size_t SIZE, std::size_t IRWIN_NUM = 12>
+		static constexpr std::array<T, SIZE> normal_distribution();
+
+	/**
+	 * @brief       Encrypts string at comple time
+	 *
+	 * @param       initial_string  The initial string
+	 * @param       string_length   Length on the initial string 
+	 * @param       key             The key for encryption
+	 * @param       key_length      Length on the encryption key
+	 *
+	 * @return      Encrypted string
+	 */
+	static constexpr const char* string_encrypt(const char* initial_string, std::size_t string_length, const char* key, std::size_t key_length)
+	{
+		std::size_t new_buffer_length = string_length + 2 + (string_length % 2);
+		int offset = 0;
+
+		char* encryptred_data = new char[new_buffer_length];
+		char* encryption_buffer = new char[new_buffer_length]; 
+
+		for(std::size_t index = 0; index < new_buffer_length; ++index)
+	    {
+	    	if (index == (new_buffer_length - 1) / 2)
+	    	{
+	    		encryption_buffer[index + offset] = key[new_buffer_length % key_length] ^ key[0];
+	    		offset += 1;
+	    	}
+	    	else 
+	    	{
+	        	encryption_buffer[index] = initial_string[index - offset];
+	        }
+	    }
+
+	    for(std::size_t index = 0; index < new_buffer_length; index += 2)
+	    {
+	    	encryptred_data[index] 		= (encryption_buffer[index] & 0b10101010) + (encryption_buffer[index + 1] & 0b01010101);
+	    	encryptred_data[index + 1]	= (encryption_buffer[index] & 0b01010101) + (encryption_buffer[index + 1] & 0b10101010);
+	    }
+
+	    for(std::size_t index = 0; index < new_buffer_length; ++index)
+	    {
+	    	encryptred_data[index] = encryptred_data[index] ^ (key[index % (key_length / sizeof(char))] + index);
+	    }
+
+	    delete[] encryption_buffer;
+	  
+	    encryption_buffer = nullptr;
+
+	    return encryptred_data;
+	}
+
+	/**
+	 * @brief       Decrypts string at comple time
+	 *
+	 * @param       initial_string  The initial string
+	 * @param       string_length   Length on the initial string 
+	 * @param       key             The key for decryption
+	 * @param       key_length      Length on the decryption key
+	 *
+	 * @return      Decrypts string
+	 */
+	static const char* string_decrypt(const char* initial_string, std::size_t string_length, const char* key, std::size_t key_length)
+	{
+		std::size_t new_buffer_length = string_length + 2 + (string_length % 2);
+		int offset = 0;
+
+		char* decryptred_data = new char[string_length];
+		char* decryptred_buffer = new char[new_buffer_length]; 
+		char* decryptred_mid_buffer = new char[new_buffer_length]; 
+
+		for(std::size_t index = 0; index < new_buffer_length; ++index)
+	    {
+	    	decryptred_buffer[index] = initial_string[index] ^ (key[index % (key_length / sizeof(char))] + index);
+	    }
+
+	    for(std::size_t index = 0; index < new_buffer_length; index += 2)
+	    {
+	    	decryptred_mid_buffer[index] 		= (decryptred_buffer[index] & 0b10101010) + (decryptred_buffer[index + 1] & 0b01010101);
+	    	decryptred_mid_buffer[index + 1]	= (decryptred_buffer[index] & 0b01010101) + (decryptred_buffer[index + 1] & 0b10101010);
+	    }
+
+		for(std::size_t index = 0; index <= string_length; ++index)
+	    {
+	    	if ((index + offset + 1) == (new_buffer_length - 1) / 2)
+	    	{
+	    		offset += 1;
+	    	}
+	    	else 
+	    	{
+	        	decryptred_data[index] = decryptred_mid_buffer[index + offset];
+	        }
+	    }
+
+	    delete[] decryptred_buffer;
+	    delete[] decryptred_mid_buffer;
+
+	    decryptred_mid_buffer = nullptr;
+	    decryptred_buffer 	  = nullptr;
+
+	    return decryptred_data;
+	}
+
+private:
+	static constexpr unsigned int  constexpr_ctoi(const char* str, int offset);
+	static constexpr unsigned int  constexpr_seed();
+	static constexpr std::uint32_t uniform_distribution(std::uint32_t &previous);
+	static constexpr double uniform_distribution_n(std::uint32_t &previous);
+
+protected:
+
+	constexpr static const std::uint32_t lce_a = 4096;
+	constexpr static const std::uint32_t lce_c = 150889;
+	constexpr static const std::uint32_t lce_m = 714025;
+};
+
+
+#ifndef __CGUI_OBF_KEY__
+	#define __CGUI_OBF_KEY__
+#endif
+
+#define __CGUI_OBF_GENERIC__(data, key) \
+	[](){ \
+		constexpr auto str_len = (__builtin_strlen(data)); \
+		constexpr auto key_len = (__builtin_strlen(key)); \
+		constexpr auto encrypted_data = ObfuscatedString::string_encrypt(data, str_len, key, key_len); \
+		thread_local const char* obfuscated_data = ObfuscatedString::string_decrypt(encrypted_data, str_len, key, key_len); \
+		return obfuscated_data; \
+	}()
+
+#define __CGUI_OBF__
+
+#define __CGUI_OBF_S__
+
+#endif // OBFUSCATED_STRING_HPP
