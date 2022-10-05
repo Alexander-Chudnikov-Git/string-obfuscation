651aa7bbe68f6da6af5e951bca8847d8aa1d39c6
THE-CHOODICK <84818316+THE-CHOODICK@users.noreply.github.com>
2022-10-05 04:41:03 +0300

    Minor Improvements

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include "string/string_lib.hpp"
 #include <iostream>
-#include <bitset>
 
 int main()
 {
 	std::cout << __CGUI_OBF__("Some long string, that should be encrypted") << std::endl;
+	std::cout << __CGUI_OBF__("Some long string, that should be encrypted") << std::endl;
+	std::cout << __CGUI_OBF__("Some long string, that should be encrypted") << std::endl;
+	std::cout << __CGUI_OBF__("Some long string, that should be encrypted") << std::endl;
+	
 	std::cout << __CGUI_OBF_S__("Some long string, that should be encrypted") << std::endl;
+	std::cout << __CGUI_OBF_S__("Some long string, that should be encrypted") << std::endl;
+	std::cout << __CGUI_OBF_S__("Some long string, that should be encrypted") << std::endl;
+	std::cout << __CGUI_OBF_S__("Some long string, that should be encrypted") << std::endl;
+	
 	return 0;
 }
